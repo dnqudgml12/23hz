5934b680f025a8021f3d113852baff3636295235
Kim-Hyun-Seung <120882082+Kim-Hyun-Seung@users.noreply.github.com>
2023-05-19 23:38:41 +0900

    Loading 생성

--- change/src/components/Eat.js
+++ change/src/components/Eat.js
@@ -1,9 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+
+const Loading = styled.div`
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  border: 10px solid rgba(2,255,255,.3);
+  border-radius: 50%;
+  border-top-color: #fff;
+  animation: spin 1s ease-in-out infinite;
+  -webkit-animation: spin 1s ease-in-out infinite;
+
+@keyframes spin {
+  to { -webkit-transform: rotate(360deg); }
+}
+@-webkit-keyframes spin {
+  to { -webkit-transform: rotate(360deg); }
+}
+`; 
 
 const Eat=()=>{
 
     return(
+        <div>
         <div>Eat</div>
+        <Loading/>
+        </div>
     )
 }
 
