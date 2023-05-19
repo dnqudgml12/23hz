a3a5571b4a26815572d9dccfd0cb06606f23cd76
dnqudgml12 <21800454@handomg.ac.kr>
2023-05-20 02:25:22 +0900

    Text

--- change/src/components/Tetx.js
+++ change/src/components/Tetx.js
@@ -3,6 +3,12 @@ import { Link} from 'react-router-dom';
 import {useNavigate} from 'react-router-dom'
 import styled from 'styled-components';
 const Text = () => {
+
+    const Div= styled.div`
+
+    margin: 0px auto;
+    
+    `
    
     const [come, setcome] = useState(false);
     const category = ['선택','결혼','연애' ,'취업', '성적', '돈'];
@@ -19,6 +25,12 @@ const Text = () => {
         
     }
 
+    const Form= styled.form`
+    
+    margin-left: 800px;
+    margin-top: 50px;
+    `
+
     const Select= styled.select`
     
     color: black;
@@ -30,14 +42,81 @@ const Text = () => {
     
     color: blue;
     :hover{
-        color: brown;
+        background-color: red;
     }
     
     `
 
+     const Processbar= styled.div`
+     display: flex;
+     margin-left: 800px;
+     margin-top: 100px;
+     align-items: center;
+
+
+
+     width: 100%;
+     height: 24px;
+  
+     
+     `
+
+    const Circle1 = styled.div`
+  
+     width: 24px;
+height: 24px;
+     background: #FD3C2D;
+/* black */
+border-radius: 50%;
+border: 2px solid #222222;
+     `
+
+
+
+    const Line1 = styled.div`
+       
+     border: 2px solid #222222;
+     width: 149px;
+    height: 0px;
+     `
+    const Circle2 = styled.div`
+          
+          width: 24px;
+     height: 24px;
+          background: #FD3C2D;
+     /* black */
+     border-radius: 50%;
+     border: 2px solid #222222;
+          `
+    const Line2 = styled.div`
+                
+               border: 2px solid #222222;
+               width: 149px;
+          height: 0px;
+               `
+    const Circle3 = styled.div`
+            border-radius: 50%;
+               width: 24px;
+          height: 24px;
+               background: #FD3C2D;
+          /* black */
+          
+          border: 2px solid #222222;
+               `
+
 
     return(
-        <div>
+        <Div>
+            <Processbar>
+                <Circle1/>
+            <Line1/>
+            <Circle2/>
+            <Line2/>
+            <Circle3/>
+                 </Processbar>
+
+
+            <Form>
             <Select onChange={onChange}>
                 {category.map((cate, index) =>
                     <Option key={index} value={cate}>{cate}
@@ -57,9 +136,12 @@ const Text = () => {
             : null}
 
            <button><Link to='/'>Home</Link></button> 
-            
-
-        </div>
+           
+           </Form>
+           
+          
+        </Div>
+      
     );
 };
 
