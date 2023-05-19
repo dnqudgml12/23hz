186ed01b6b27fd0f0b42b99ec5ff9b2ceb917af3
dnqudgml12 <21800454@handomg.ac.kr>
2023-05-20 01:06:20 +0900

    text

--- change/src/components/Tetx.js
+++ change/src/components/Tetx.js
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Link} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom'
+import styled from 'styled-components';
+const Text = () => {
+   
+    const [come, setcome] = useState(false);
+    const category = ['선택','결혼','연애' ,'취업', '성적', '돈'];
+    const navigate= useNavigate();
 
-const Text=()=>{
+    const onChange = (e) => {
+        const selectedbox= e.target.value;
+        if (selectedbox === '선택' ){
+            setcome(false);
+        } else{
+            setcome(true);
+        }
+
+        
+    }
+
+    const Select= styled.select`
+    
+    color: black;
+
+    
+    `
+    
+    const Option= styled.option`
+    
+    color: blue;
+    :hover{
+        color: brown;
+    }
+    
+    `
+
 
     return(
-        <div>Text</div>
-    )
-}
+        <div>
+            <Select onChange={onChange}>
+                {category.map((cate, index) =>
+                    <Option key={index} value={cate}>{cate}
+                    
+                    </Option>
+                )}
+            </Select>
+            {come === true ? 
+                <form  onSubmit={(e)=>{
+                    e.preventDefault();
+                   navigate('/');
+                    
+                }}>
+                    <textarea placeholder='당신의 걱정을 입력하세요'></textarea>
+                    <input type='submit' value='제출'/>
+                </form>
+            : null}
+
+           <button><Link to='/'>Home</Link></button> 
+            
+
+        </div>
+    );
+};
 
 export default Text;
